f11ff2b8db50d0001d93795f6a8722396acc124a
Kent Tokunaga <kenttokunaga@gmail.com>
2019-01-27 18:21:44 -0500

    Display board

--- src/components/TicTacToe/TicTacToe.js
+++ src/components/TicTacToe/TicTacToe.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import './TicTacToe.css';
 
-export default class TicTacToe extends Component {
+class TicTacToe extends Component {
   render() {
     return (
-      <div>
-        TicTacToe
+      <div className="tic-tac-toe">
+        <div className="ttt-cell ttt-top-left">O</div>
+        <div className="ttt-cell ttt-top-center">O</div>
+        <div className="ttt-cell ttt-top-right">O</div>
+        <div className="ttt-cell ttt-center-left">O</div>
+        <div className="ttt-cell ttt-bottom-left">O</div>
+        <div className="ttt-cell ttt-center-center">O</div>
+        <div className="ttt-cell ttt-center-right">O</div>
+        <div className="ttt-cell ttt-bottom-center">O</div>
+        <div className="ttt-cell ttt-bottom-right">O</div>
       </div>
     );
   }
 }
+
+export default TicTacToe;
