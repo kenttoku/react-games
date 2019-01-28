8a1b6948732722697aa812437c24b5c3265a2297
Kent Tokunaga <kenttokunaga@gmail.com>
2019-01-28 16:29:00 -0500

    Use bootstrap for borders

--- src/components/TicTacToe/TicTacToe.js
+++ src/components/TicTacToe/TicTacToe.js
@@ -9,24 +9,29 @@ class TicTacToe extends Component {
     const cells = board.map((cell, index) => {
       let column;
       let row;
+      let borderClasses = 'border border-dark';
 
       if (index % 3 === 0) {
         column = 'left';
+        borderClasses += ' border-left-0';
       } else if (index % 3 === 1) {
         column = 'center';
       } else {
         column = 'right';
+        borderClasses += ' border-right-0';
       }
 
       if (Math.floor(index / 3) === 0) {
         row = 'top';
+        borderClasses += ' border-top-0';
       } else if (Math.floor(index / 3) === 1) {
         row = 'center';
       } else {
         row = 'bottom';
+        borderClasses += ' border-bottom-0';
       }
 
-      return <div key={`ttt-${row}-${column}`} className={`ttt-cell ttt-${row}-${column}`}>{cell}</div>;
+      return <div key={`ttt-${row}-${column}`} className={`ttt-cell ttt-${row}-${column} ${borderClasses}`}>{cell}</div>;
     });
 
     return (
